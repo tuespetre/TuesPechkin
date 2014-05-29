b2d10ba411b5b1ac4bb0abdccc290b2fb7e1f14b
tuespetre <dcgray@yeahbaby.fake>
2014-05-28 23:50:22 -0500

    Added sample MVC web app

---
+++ TuesPechkin.TestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Pechkin;
+
+namespace TuesPechkin.TestWebApp.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        //
+        // GET: /Home/
+        public async Task<ActionResult> Index()
+        {
+            var html = "<p>Just some test HTML</p>";
+
+            for (var i = 0; i < 5; i++)
+            {
+                var converter = Factory.Create(new GlobalConfig());
+                converter.Convert(html);
+            }
+
+            return this.View();
+        }
+    }
+}
