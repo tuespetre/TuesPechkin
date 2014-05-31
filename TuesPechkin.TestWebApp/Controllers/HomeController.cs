a3bf514fa42a6626757033441d49b53cb6a98b99
tuespetre <dcgray@yeahbaby.fake>
2014-05-31 16:13:44 -0500

    Changed the test MVC app a bit

--- TuesPechkin.TestWebApp/Controllers/HomeController.cs
+++ TuesPechkin.TestWebApp/Controllers/HomeController.cs
@@ -1,30 +1,24 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace TuesPechkin.TestWebApp.Controllers
 {
-    public class HomeController : AsyncController
+    public class HomeController : Controller
     {
         //
         // GET: /Home/
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
             var html = "<p>Just some test HTML</p>";
 
             for (var i = 0; i < 5; i++)
             {
                 var converter = Factory.Create();
+                var result = converter.Convert(html);
                 var path = this.Server.MapPath(String.Format("/{0}.pdf", i));
 
-                using (var file = System.IO.File.Open(path, FileMode.Create))
-                {
-                    var result = new MemoryStream(converter.Convert(html));
-
-                    result.CopyTo(file);
-                }
+                System.IO.File.WriteAllBytes(path, result);
             }
 
             return this.View();
