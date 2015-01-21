9d1ad1a77e4c8da7fbfd4a14bdacb3113d93e107
Derek Gray <dgray@nex-tech.com>
2015-01-21 09:12:52 -0600

    Bump version

--- TuesPechkin/Properties/AssemblyInfo.cs
+++ TuesPechkin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET wrapper for wkhtmltopdf; supports 32-bit, 64-bit, multi-threaded and IIS environments.")]
 [assembly: AssemblyProduct("TuesPechkin")]
 [assembly: AssemblyCopyright("Copyright 2014 Derek Gray")]
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("11f63696-7105-436d-9ec6-2fee54c40b11")]
